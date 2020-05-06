2c363937b68c4e9ce910eafebc7e1be5c011efa0
Aditya Bhardwaj <greytopihacker@gmail.com>
2020-05-06 07:19:19 +0530

    Added GSON, check for login

--- src/main/java/com/network_traffic/XHR_capture/XHRCapture.java
+++ src/main/java/com/network_traffic/XHR_capture/XHRCapture.java
@@ -7,6 +7,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,67 +23,86 @@ import net.lightbody.bmp.core.har.Har;
 import net.lightbody.bmp.proxy.CaptureType;
 
 public class XHRCapture {
-	
-	//read from local properties file
+
+	// read from local properties file
 	String driverPath = "driverPath/location";
 	String harFileName = "har_file_name";
 	String user_data_directory = "user_data_directory_location";
 	public WebDriver driver;
 	public BrowserMobProxy proxy;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		
-	   // start the proxy
-	    proxy = new BrowserMobProxyServer();
-	    proxy.setTrustAllServers(true);
-	    proxy.start(0);
 
-	    //get the Selenium proxy object - org.openqa.selenium.Proxy;
-	    Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
-	    
-	    // configure it as a desired capability
-	    DesiredCapabilities capabilities = new DesiredCapabilities();
-	    
-	    capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
-	    
-	    ChromeOptions options = new ChromeOptions();
-	    options.addArguments("--ignore-certificate-errors","--user-data-dir=user_data_directory");
-	    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-	    
-		
-	    //set chromedriver system property
-		System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
+		// start the proxy
+		proxy = new BrowserMobProxyServer();
+		proxy.setTrustAllServers(true);
+		proxy.start(0);
+
+		// get the Selenium proxy object - org.openqa.selenium.Proxy;
+		Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
+		// configure it as a desired capability
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--ignore-certificate-errors", "--user-data-dir=user_data_directory");
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+		// set chromedriver system property
+		System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
 		driver = new ChromeDriver(capabilities);
+
+		// enable more detailed HAR capture, if desired (see CaptureType for the
+		// complete list)
+		proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+
+		// create a new HAR
+		proxy.newHar("preferred_file_name");
+
+		// open website
+		driver.get("website_link");
+
+		WebElement username = null;
+		WebElement password = null;
+		boolean loggedIn = false;
+
+		/*
+		 * Check whether it can find username/password element to see if it's
+		 * already logged -> Better way will be to check against the website
+		 * title -> or some other in-built method
+		 */
+		try {
+			username = driver.findElement(By.id("username"));
+			password = driver.findElement(By.id("password"));
+		} catch (NoSuchElementException e) {
+			loggedIn = true;
+			System.out.println("Already logged in to the website");
+			Thread.sleep(10000);
+			driver.get("another_webpage_link");
+			Thread.sleep(5000);
+
+		}
 		
-	    // enable more detailed HAR capture, if desired (see CaptureType for the complete list)
-	    proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-
-	    // create a new HAR
-	    proxy.newHar("preferred_file_name");
-
-	    // open website
-	    driver.get("website_link");
-	    
-	    //login
-	    WebElement username = driver.findElement(By.id("username"));
-	    WebElement password = driver.findElement(By.id("password"));
-	    
-        username.sendKeys("your_username");
-        password.sendKeys("your_password");
-        driver.findElement(By.id("loginbtn")).click();
-        Thread.sleep(10000);
-        driver.get("another_webpage_link");
-        Thread.sleep(5000);
+		if (!loggedIn) {
+			username.sendKeys("your_username");
+			password.sendKeys("your_password");
+			driver.findElement(By.id("loginbtn")).click();
+			Thread.sleep(10000);
+			driver.get("another_webpage_link");
+			Thread.sleep(5000);
+		}
 	}
-	
+
 	@Test
 	public void testCaseOne() throws Exception {
 		System.out.println("Navigating...");
 		Thread.sleep(10000);
 		System.out.println(driver.getTitle());
 	}
-	
+
 	@After
 	public void tearDown() {
 
@@ -94,16 +114,19 @@ public class XHRCapture {
 		try {
 			har.writeTo(harFile);
 		} catch (IOException ex) {
-			 System.out.println (ex.toString());
-		     System.out.println("Could not find file " + harFileName);
+			System.out.println(ex.toString());
+			System.out.println("Could not find file " + harFileName);
 		}
 		
+		//Write har content to JSON file using GSON
+		
+		//TODO
+
 		if (driver != null) {
 			proxy.stop();
 			driver.quit();
 		}
-		
+
 		System.out.println("Done");
 	}
 }
-
